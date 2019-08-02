e67d5e9e2f963895b532af4a5d838dee93ef7600
Raul Dias Barboza <52804741+serious-raul@users.noreply.github.com>
2019-08-01 23:32:44 -0300

    now it is perfect
    actually is missing audios response

--- myFunction.js
+++ myFunction.js
@@ -17,3 +17,22 @@ function showMore(rets, more, button) {
         moreText.style.display = "inline";
     }
 }
+
+
+function showMoreList(rets, more, button) {
+
+    var dots = document.getElementById(rets);
+    var btnText = document.getElementById(button);
+    var moreText = document.getElementById(more);
+
+    if (dots.style.display === "contents") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "▼";
+        moreText.style.display = "none";
+    }
+    else {
+        dots.style.display = "contents";
+        btnText.innerHTML = "▲";
+        moreText.style.display = "block";
+    }
+}
